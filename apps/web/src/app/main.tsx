33d8e5ae69174032f80f4cd652e40ef945d08f12
Zhora Asatryan <as.zh.mi.27@gmail.com>
2026-03-27 19:59:19 +0400

    chore: Introduce a new UI package with shadcn under @repo/ui

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-import "@repo/ui/globals.css";
 import { ThemeProvider } from "./providers/theme-provider";
+import "@repo/ui/globals.css";
+import { LoginForm } from "@/shared/ui/components/login-form";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
@@ -10,7 +10,17 @@ if (!rootElement) throw new Error("Root element not found");
 createRoot(rootElement).render(
   <StrictMode>
     <ThemeProvider>
-      <div></div>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            Acme Inc.
+          </a>
+          <LoginForm />
+        </div>
+      </div>
     </ThemeProvider>
   </StrictMode>,
 );
