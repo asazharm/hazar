4338f232f16fe52594d525804138ffdc7464aa7d
Zhora Asatryan <as.zh.mi.27@gmail.com>
2026-03-29 00:57:45 +0400

    chore: apply biome rules

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -8,19 +8,19 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Root element not found");
 
 createRoot(rootElement).render(
-  <StrictMode>
-    <ThemeProvider>
-      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
-          <a
-            href="#"
-            className="flex items-center gap-2 self-center font-medium"
-          >
-            Acme Inc.
-          </a>
-          <LoginForm />
-        </div>
-      </div>
-    </ThemeProvider>
-  </StrictMode>,
+	<StrictMode>
+		<ThemeProvider>
+			<div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+				<div className="flex w-full max-w-sm flex-col gap-6">
+					<a
+						href="#"
+						className="flex items-center gap-2 self-center font-medium"
+					>
+						Acme Inc.
+					</a>
+					<LoginForm />
+				</div>
+			</div>
+		</ThemeProvider>
+	</StrictMode>,
 );
